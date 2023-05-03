57c03c81d44cbc4b439a0a51b7ef8b1754995f88
kirsikat <114054084+kirsikat@users.noreply.github.com>
2023-05-03 17:47:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const form = document.querySelector('form'); 
+const input = document.querySelector('input'); 
+const resultsDiv = document.querySelector('#results'); 
+ 
+form.addEventListener('submit', function(event) { 
+  event.preventDefault(); 
+ 
+  const searchTerm = input.value; 
+  resultsDiv.innerHTML = <p>Результати для "${searchTerm}" будуть відображені тут</p>; 
+});
 document.getElementById('tab1').style.display = 'block';
 function openTab(evt, tabName) {
   let i, tabcontent, tablinks;
